87ca86141c7f903747e1cb48276490d42b4f77b2
HarryUnth <h.unthank-23@student.lboro.ac.uk>
2026-02-06 20:28:21 +0000

    Added Bezier function for curved lines between waypoints

--- Assets/WaypointMover.cs
+++ Assets/WaypointMover.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class WaypointMover : MonoBehaviour
@@ -7,9 +8,28 @@ public class WaypointMover : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 5f;
 
+    [Range(0f, 15f)] //How fast the agent will rotate once it reaches its waypoint
+    [SerializeField] private float rotateSpeed = 10f;
+
     [SerializeField] private float distanceThreshold = 0.1f;
 
+    [SerializeField] private float curveStrength = 2f;
+
+    private Transform previousWaypoint;
+
+    private float t;
+
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+
     private Transform currentWaypoint;
+
+    //The rotation target for the current frame
+    private Quaternion rotationGoal;
+
+    //The direction to the next waypoint that the agent needs to rotate towards
+    private Vector3 directionToWaypoint;
+
 
 
 
@@ -30,7 +50,24 @@ public class WaypointMover : MonoBehaviour
         if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
         {
             currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
-            transform.LookAt(currentWaypoint);
+            //transform.LookAt(currentWaypoint);
         }
+        RotateTowardsWaypoint();
+    }
+
+    //Will slowly rotate the agent towards the current waypoint it is moving towards
+    private void RotateTowardsWaypoint()
+    {         
+        directionToWaypoint = (currentWaypoint.position - transform.position).normalized;
+        rotationGoal = Quaternion.LookRotation(directionToWaypoint);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationGoal, rotateSpeed * Time.deltaTime);
+    }
+
+    private Vector3 Bezier(Vector3 a, Vector3 b, Vector3 c, float t)
+    {
+        Vector3 ab = Vector3.Lerp(a, b, t);
+        Vector3 bc = Vector3.Lerp(b, c, t);
+
+        return Vector3.Lerp(ab, bc, t);
     }
 }
--- Assets/Waypoints.cs
+++ Assets/Waypoints.cs
@@ -5,6 +5,13 @@ public class Waypoints : MonoBehaviour
     [Range(0f, 2f)]
     [SerializeField] private float waypointSize = 1f;
 
+    [Header("Path Settings")]
+    //Sets the path to be looped so agent will go from last waypoint to first waypoint or vice versa
+    [SerializeField] private bool canLoop = true;
+
+    //Sets the agent to move forward or backwards
+    [SerializeField] private bool isMovingForward = true;
+
     private void OnDrawGizmos()
     {
         foreach(Transform t in transform)
@@ -19,9 +26,15 @@ public class Waypoints : MonoBehaviour
             Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i + 1).position);
         }
 
-        Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position, transform.GetChild(0).position);
+        //If the path is set to loop then draw the line between the last and first waypoint
+        if (canLoop)
+        {
+            Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position, transform.GetChild(0).position);
+        }
+
     }
 
+    //Will get the correct next waypoint based on the direction currently travelling
     public Transform GetNextWaypoint(Transform currentWaypoint)
     {
         if (currentWaypoint == null)
@@ -29,13 +42,57 @@ public class Waypoints : MonoBehaviour
             return transform.GetChild(0);
         }
         
-        if (currentWaypoint.GetSiblingIndex() < transform.childCount - 1)
+        //Stores the index of the current waypoint
+        int currentIndex = currentWaypoint.GetSiblingIndex();
+
+        //Stores the index of the next waypoint to travel towards
+        int nextIndex = currentIndex;
+
+
+        //Agent is moving forward on the path
+        if (isMovingForward)
         {
-            return transform.GetChild(currentWaypoint.GetSiblingIndex() + 1);
+            nextIndex += 1;
+
+            //If the next waypoint index is equal to the count of the children/waypoints then it is already at the last waypoint
+            //Check if the path is set to loop and return the first waypoint as the current waypoint, otherwise we subtract 1 from the nextIndex which will
+            //return the same waypoint that the agent is currently at, which will cause it to stop moving since it is already there.
+
+            if (nextIndex >= transform.childCount)
+            {
+                if (canLoop)
+                {
+                    nextIndex = 0;
+                }
+                else
+                {
+                    nextIndex -= 1;
+                }
+            }
         }
+        //Agent is moving backwards on the path
         else
         {
-            return transform.GetChild(0);
+            nextIndex -= 1;
+
+            //If the nextIndex is below 0 then it means that you are already at the first waypoint, check if the path is set to loop and if so return the last waypoint
+            //otherwise add 1 to the nextIndex which will return the current waypoint that you are already at which will casue the agent to stop since it is already there.
+
+            if (nextIndex < 0)
+            {
+                if (canLoop)
+                {
+                    nextIndex = transform.childCount - 1;
+                }
+                else
+                {
+                    nextIndex += 1;
+                }
+            }
         }
+
+        //Return the waypoint that has the index of nextIndex.
+        return transform.GetChild(nextIndex);
+
     }
 }
